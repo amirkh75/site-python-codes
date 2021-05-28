8585ad1729dea307f68101d36ebb393cf285db16
amirkh75 <amirp.1375@gmail.com>
2021-05-28 12:53:41 +0430

    Added dcoker to project.

---
+++ .my_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amirkh/Documents/django_projects/python-codes.ir/.my_env/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
