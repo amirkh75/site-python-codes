f8cc087ba360da51ceed99d5ae284a479e9fa4d3
amirkh75 <amirp.1375@gmail.com>
2021-05-29 10:59:56 +0430

    Added static and mediafiles handeler and ready product.

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #local
     'users',
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
